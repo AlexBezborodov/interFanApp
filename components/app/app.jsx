792170b0340c85a3ec4d17bfc28cc6dc77f1e4bb
Alex Bezborodov <alexbezborodov23@gmail.com>
2022-10-03 18:24:46 +0300

    Fix: add theme config

--- components/app/app.jsx
+++ components/app/app.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import { NativeRouter, Routes, Route } from 'react-router-native';
+import { Login } from '../login/login';
 import { Main } from '../main/main';
 import { Splashscrenn } from '../splashscreen';
+import {
+  Provider as PaperProvider,
+  DefaultTheme,
+  Colors,
+} from "react-native-paper";
 
 export const Application = () => {
-    return (
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: Colors.blue500,
+      background: Colors.white,
+      accent: "yellow",
+      success: Colors.green500,
+      activeChoice: Colors.blue50,
+    },
+  };
+  return (
+    <PaperProvider theme={theme}>
       <NativeRouter>
-            <Routes>
-                <Route path='/' element={<Splashscrenn />} />
-                <Route path='/main' element={<Main />} />
-            </Routes>
+        <Routes>
+          <Route path='/' element={<Splashscrenn />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/main' element={<Main />} />
+        </Routes>
       </NativeRouter>
+    </PaperProvider>
+      
       
   )
 }
