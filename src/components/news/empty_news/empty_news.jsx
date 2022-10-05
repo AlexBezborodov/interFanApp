36fafd74f5912ec312ebb07d4668fed0c68a3881
Alex Bezborodov <alexbezborodov23@gmail.com>
2022-10-05 15:02:39 +0300

    Feature: add empty news component

---
+++ src/components/news/empty_news/empty_news.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from "prop-types";
+import { Box } from '../../basic_components';
+import {Title } from 'react-native-paper';
+
+export const EmptyNews = () => {
+  return (
+      <Box>
+        <Title>No news</Title>
+      </Box>
+  )
+}
