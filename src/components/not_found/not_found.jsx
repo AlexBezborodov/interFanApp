a0b12d646c7053cd4013a40694080c42725baa70
Alex Bezborodov <alexbezborodov23@gmail.com>
2022-10-05 17:41:15 +0300

    Feature: update not_found component

--- src/components/not_found/not_found.jsx
+++ src/components/not_found/not_found.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 
 import PropTypes from "prop-types";
-import { Appbar } from "react-native-paper";
-import { useNavigate } from "react-router-native";
+import { Appbar, Subheading } from "react-native-paper";
+import { useLocation, useNavigate } from "react-router-native";
 
 export function NotFound() {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
   return (
-    <Appbar.Header>
-      <Appbar.BackAction onPress={() => navigate("/news")} />
-      <Appbar.Content title="Back to the app" />
-    </Appbar.Header>
+    <>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={() => navigate("/news")} />
+        <Appbar.Content title="Back to the app" />
+      </Appbar.Header>
+      <Subheading>{pathname}</Subheading>
+    </>
   );
 }
 
