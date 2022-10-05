cc2e018f439bd9dc0ae113f633daf5867183487f
Alex Bezborodov <alexbezborodov23@gmail.com>
2022-10-05 11:47:43 +0300

    Feture: add fake data and logic for login

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
-import { ImageBackground } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { Alert, ImageBackground } from 'react-native';
 import { Title, Button, Colors } from 'react-native-paper';
 import { useNavigate } from 'react-router-native';
 import { Box } from '../basic_components/box';
 import { useStyles } from "./styles";
 import { BasicInput } from "../inputs/basic_input";
+import { useStateInputHandler } from "../../hooks/use_input_handler/use_input_handler";
+import { CurrentUserContext } from '../../providers/current_user_provider';
 
 
 export const Login = () => {
     const navigate = useNavigate()
     const image = `https://mfiles.alphacoders.com/973/973553.png`;
+    const mockLoginData = { email: "abe@test.com", password: "q1w2e3r4" };
+
+    const { setCurrentUser } = useContext(CurrentUserContext);
+
+    const [values, setValues] = useState({});
+    const { inputHandler } = useStateInputHandler(setValues);
+
+    const fakeFetch = (data) => {
+        const { email, password } = mockLoginData;
+        if (email === data.email.toLowerCase() && password === data.password) {
+            return {status: 200, user: getMockUser()}
+        } else {
+            return {status: 404, error: "Wrong email or password"}
+        }
+    }
+
+    const login = () => {
+        const res = fakeFetch(values);
+        setTimeout(() => {
+            if (res.status === 200) {
+                setCurrentUser(res.user);
+                navigate("/news");
+                setValues({});
+            } else {
+                Alert.alert(res.error);
+            }
+        }, 500);
+        
+    }
+
     return (
         
         <ImageBackground source={{ uri: image}} resizeMode="cover" style={{ width: '100%', height: '100%', alignItems:'center', justifyContent:'center'}}>
@@ -18,13 +50,24 @@ export const Login = () => {
                 <Box borderRadius={12} p={8} backgroundColor="white" width="80%">
                     {LabelLogin()}
                     <Box py={8}>
-                        <BasicInput name="email" label="Email*"/>
+                        <BasicInput
+                            name="email"
+                            label="Email*"
+                            value={values?.email || ""}
+                            onChangeText={inputHandler}
+                             />
                     </Box>
                     <Box py={8}>
-                        <BasicInput label="Password*"/>
+                        <BasicInput
+                            secureTextEntry={true}
+                            name="password"
+                            label="Password*"
+                            value={values?.password || ""}
+                            onChangeText={inputHandler}
+                        />
                     </Box>
                     <Box py={8}>
-                        <Button onPress={() => navigate('/news')}><Title>En</Title><Title style={{ color: Colors.blue600}}>ter</Title></Button>
+                        <Button onPress={login}><Title>En</Title><Title style={{ color: Colors.blue600}}>ter</Title></Button>
                     </Box>
                 </Box>
             </Box>
@@ -46,4 +89,18 @@ const LabelLogin = () => {
         </Box>
     )
     
+}
+
+function getMockUser() {
+    return {
+        id: 1235532943358769,
+        name: "Alex",
+        surname: "Bezborodov",
+        birthDate: "23.11.1987",
+        nickName: "ABE",
+        email: "abe@test.com",
+        phone: "0987654321",
+        country: "Ukraine",
+        city: "Che",
+    }
 }
