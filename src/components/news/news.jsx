81177486f2d3cba151906e0a791053748729c5e6
Alex Bezborodov <alexbezborodov23@gmail.com>
2022-10-05 15:03:38 +0300

    Feature: add posts to news

--- src/components/news/news.jsx
+++ src/components/news/news.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import { Box } from "../basic_components/box";
-import { Title } from 'react-native-paper';
+import React, { useEffect, useState } from 'react';
 import { AppHeader } from '../header/header';
 import { Layout } from '../layout'
 import { Colors } from 'react-native-paper';
+import { ShortArticle } from './short_article';
+import { ScrollView } from 'react-native';
+import { EmptyNews } from './empty_news'
 
 export const News = () => {
+    const [news, setNews] = useState([]);
+    const fetchNews = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(json => setNews(json))
+    }
+
+    useEffect(() => {
+        fetchNews();
+    },[])
     return (
         <>
-        <AppHeader title="News" bgc={Colors.blueGrey800} />
-        <Layout>
-            <Box justifyContent="center" alignItems="center" flex={1}>
-            <Title>News</Title>   
-            </Box>  
-        </Layout>
+            <AppHeader title="News" bgc={Colors.blueGrey800} />
+            <ScrollView>
+                <Layout>  
+                    {news.length ? (news?.map(article => <ShortArticle key={article.id} {...article} />)) : (<EmptyNews />)}
+                </Layout>
+            </ScrollView>
         </>
         
   )
