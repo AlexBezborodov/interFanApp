3cc87bcf94b06b189d4553355e825be0d3064989
Alex Bezborodov <alexbezborodov23@gmail.com>
2022-10-06 11:08:57 +0300

    Feature: add snackBarProvider to app

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,7 +8,7 @@ import {
   Colors,
 } from "react-native-paper";
 import { BottomNavLayout } from '../bottom_navigation';
-import { CurrentUserProvider } from '../../providers/current_user_provider';
+import { CurrentUserProvider, SnackBarProvider } from '../../providers';
 import { NotFound } from '../not_found';
 import { FullArticle } from '../news/full_article';
 
@@ -25,20 +25,22 @@ export const Application = () => {
   };
   return (
     <PaperProvider theme={theme}>
-      <CurrentUserProvider>
-        <NativeRouter>
-          <Routes>
-            <Route path='/' element={<Splashscrenn />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/news' element={<BottomNavLayout />} />
-            <Route path='/news/:id' element={< FullArticle />}/>
-            <Route path='/chat' element={<BottomNavLayout />} />
-            <Route path='/team' element={<BottomNavLayout />} />
-            <Route path='/profile' element={<BottomNavLayout />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </NativeRouter>
-      </CurrentUserProvider>
+      <SnackBarProvider>
+        <CurrentUserProvider>
+          <NativeRouter>
+            <Routes>
+              <Route path='/' element={<Splashscrenn />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/news' element={<BottomNavLayout />} />
+              <Route path='/news/:id' element={< FullArticle />}/>
+              <Route path='/chat' element={<BottomNavLayout />} />
+              <Route path='/team' element={<BottomNavLayout />} />
+              <Route path='/profile' element={<BottomNavLayout />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </NativeRouter>
+        </CurrentUserProvider>
+      </SnackBarProvider>
     </PaperProvider>
     
       
